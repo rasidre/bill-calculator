ef25ad792911c0c0e7d3a3b4e88cd21c0e6a96fb
Rasid Redzic <rasidredzic@gmail.com>
2020-01-17 00:29:45 +0100

    Add functionality and elements to calculator

--- main.js
+++ main.js
@@ -1,11 +1,54 @@
 const root = document.getElementById("root");
 
-let calculatorContainer;
+let calculatorContainer = document.createElement("div");
+let headline = document.createElement("h2");
 
-function createAndAddElement(addTo, defineHtmlElement, overrideVar, classesAsString) {
-  overrideVar = document.createElement(defineHtmlElement);
-  overrideVar.classList.add(classesAsString);
-  addTo.appendChild(overrideVar);
+let taxDescription = document.createElement("p");
+let taxInput = document.createElement("input");
+
+let billTotalDescription = document.createElement("p");
+let billTotalInput = document.createElement("input");
+
+let calculateButton = document.createElement("button");
+let result = document.createElement("span");
+
+function splitClasses(classes) {
+  return classes.split(" ");
 }
 
-createAndAddElement(root, "div", calculatorContainer, "calculator-container");
+function addElement(addTo, htmlElement, classesAsString = "", elementText = false) {
+  let classes = splitClasses(classesAsString);
+  htmlElement.classList.add(...classes);
+  elementText ? (htmlElement.innerText = elementText) : null;
+  addTo.appendChild(htmlElement);
+}
+
+addElement(root, calculatorContainer, "calculator-container");
+addElement(calculatorContainer, headline, "headline", "Bill Calculator");
+addElement(
+  calculatorContainer,
+  taxDescription,
+  "description tax-description",
+  "Please enter your tax rate in %:"
+);
+addElement(calculatorContainer, taxInput, "input tax-input");
+addElement(
+  calculatorContainer,
+  billTotalDescription,
+  "description bill-total-description",
+  "Please enter your bill in €:"
+);
+addElement(calculatorContainer, billTotalInput, "input bill-total-input");
+addElement(calculatorContainer, calculateButton, "btn", "Calculate");
+addElement(calculatorContainer, result, "result", "Result: ");
+
+calculateButton.addEventListener("click", function() {
+  let taxValue = taxInput.value / 100;
+  let billValue = billTotalInput.value; 
+  let billTotal = billValue * (1 + taxValue);
+  let resultAfterComma = billTotal.toString().split(".")[1];
+  let resultBeforeComma = billTotal.toString().split(".")[0];
+  resultAfterModifying = resultBeforeComma + "." + resultAfterComma.slice(0,2);
+  
+  result.innerText = "Result: " + resultAfterModifying + " €";
+});
